94fe57d6ebbc8c91a35d7c33cef6bbbc9d1aaf22
Jon Connell <python@figsandfudge.com>
2024-10-12 16:09:37 +0100

    Fix typo

--- Formula/numbers-parser.rb
+++ Formula/numbers-parser.rb
@@ -1,7 +1,7 @@
 class NumbersParser < Formula
   include Language::Python::Virtualenv
 
-  desc "Read and write Apple Numbers spreadsheets
+  desc "Read and write Apple Numbers spreadsheets"
   homepage "https://github.com/masaccio/numbers-parser"
   url "https://files.pythonhosted.org/packages/f9/c0/399f1a507cd09c99aae6913d4e1298423319ef045cbbeb41f1c096798760/numbers_parser-4.13.3.tar.gz"
   sha256 "8a2af6344a230727f72e6758c562557ddbb124c07e1b49bb1fe3ba4d152f249c"
